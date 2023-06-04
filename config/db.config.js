dc4bcdcf834abe0d1954de2fd491f5433537b3d2
Umang Devanshu <devumang096@gmail.com>
2023-06-04 16:00:12 +0530

    settip up mongodb

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: "mongodb+srv://devumang096:123@#123@contact-app.3kdsoe8.mongodb.net/?retryWrites=true&w=majority"
+	url: "mongodb+srv://devumang096:123%40%23123@contact-app.3kdsoe8.mongodb.net/?retryWrites=true&w=majority"
 }
